328d54261b711d53ab23a947150c447897d182bd
Benoît Dardenne <benoit.dardenne@gmail.com>
2018-01-06 22:59:00 +0100

    Remove done TODOs

--- src/main/java/spendee/ui/charts/DonutChart.java
+++ src/main/java/spendee/ui/charts/DonutChart.java
@@ -1,25 +1,55 @@
 package spendee.ui.charts;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.DoubleBinding;
 import javafx.collections.ObservableList;
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.chart.PieChart;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
+
+import java.text.DecimalFormat;
 
 
 public class DonutChart extends PieChart {
+
+  private static final DecimalFormat FORMATTER = new DecimalFormat( "#" );
+
   private final Circle innerCircle = new Circle();
+  private TextFlow label = new TextFlow();
+  private StackPane pane = new StackPane();
+
+  private DoubleBinding totalValue = Bindings.createDoubleBinding( () -> getData().stream()
+                                                                                  .mapToDouble( Data::getPieValue )
+                                                                                  .sum(), dataProperty() );
 
   public DonutChart() {
-    innerCircle.getStyleClass().add("inner-circle");
-  }
+    innerCircle.getStyleClass().add( "inner-circle" );
+    label.setTextAlignment( TextAlignment.CENTER );
 
-  public DonutChart( ObservableList<Data> pieData ) {
-    this();
-    setData( pieData );
-  }
+    pane.getChildren().addAll( innerCircle, label );
+    pane.setMouseTransparent( true );
 
+    this.dataProperty().addListener( ( observable, oldValue, newValue ) -> {
+      newValue.forEach( slice -> slice.getNode().setOnMouseEntered( e -> {
+        double value = slice.getPieValue();
+
+        Text name = new Text( slice.getName() + "\n" );
+        Text amount = new Text( FORMATTER.format( value ) + "€\n" );
+        Text share = new Text( FORMATTER.format( 100 * slice.getPieValue() / totalValue.get() ) + "%" );
+
+        label.getChildren().addAll( name, amount, share );
+        label.setMinWidth( innerCircle.getRadius() * 1.8 );
+      } ) );
+
+      newValue.forEach( slice -> slice.getNode().setOnMouseExited( e -> label.getChildren().clear() ) );
+    } );
+  }
 
   @Override
   protected void layoutChartChildren( double top, double left, double contentWidth, double contentHeight ) {
@@ -35,8 +65,8 @@ public class DonutChart extends PieChart {
       if ( pie.getParent() instanceof Pane ) {
         Pane parent = ( Pane ) pie.getParent();
 
-        if ( !parent.getChildren().contains( innerCircle ) ) {
-          parent.getChildren().add( innerCircle );
+        if ( !parent.getChildren().contains( pane ) ) {
+          parent.getChildren().add( pane );
         }
       }
     }
@@ -64,10 +94,14 @@ public class DonutChart extends PieChart {
       }
     }
 
-    innerCircle.setCenterX( minX + ( maxX - minX ) / 2 );
-    innerCircle.setCenterY( minY + ( maxY - minY ) / 2 );
-
     innerCircle.setRadius( ( maxX - minX ) / 3 );
-    innerCircle.toFront();
+
+    double centerX = minX + ( maxX - minX ) / 2;
+    double centerY = minY + ( maxY - minY ) / 2;
+
+    pane.setTranslateX( centerX );
+    pane.setTranslateY( centerY );
+
+    pane.toFront();
   }
 }
