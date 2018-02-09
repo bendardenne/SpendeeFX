dc3683ca0a73a086abd02c766680d9e23fe7366c
Benoît Dardenne <benoit.dardenne@gmail.com>
2018-02-09 15:18:29 +0100

    Added some colours

--- src/main/java/spendee/ui/ColourProvider.java
+++ src/main/java/spendee/ui/ColourProvider.java
@@ -11,6 +11,8 @@ public class ColourProvider {
     COLOR_MAP.put( "Meal Vouchers", "#BED940" );
     COLOR_MAP.put( "Reimbursement", "#18B272" );
     COLOR_MAP.put( "BlaBlaCar", "#47A7E6" );
+    COLOR_MAP.put( "Extra income", "#1EADCE" );
+
     COLOR_MAP.put( "Car", "#47A7E6" );
     COLOR_MAP.put( "Savings", "#6D6E89" );
     COLOR_MAP.put( "Rent", "#DE8135" );
@@ -19,6 +21,7 @@ public class ColourProvider {
     COLOR_MAP.put( "Groceries", "#B47B55" );
     COLOR_MAP.put( "Take Away & Restaurant", "#B9965E" );
     COLOR_MAP.put( "Drinks", "#DF8C29" );
+    COLOR_MAP.put( "Beer", "#F5534B" );
     COLOR_MAP.put( "Brewing", "#18B272" );
     COLOR_MAP.put( "Utilities", "#61708C" );
     COLOR_MAP.put( "Transportation", "#FFCC00" );
@@ -26,14 +29,18 @@ public class ColourProvider {
     COLOR_MAP.put( "Music", "#FFA800" );
     COLOR_MAP.put( "Reading", "#5CC5AC" );
     COLOR_MAP.put( "Games", "#3D75AB" );
+    COLOR_MAP.put( "Cinema", "#47A7E6" );
     COLOR_MAP.put( "Accommodation", "#6E8CB1" );
     COLOR_MAP.put( "Healthcare", "#E56274" );
+    COLOR_MAP.put( "Clothing", "#BED940" );
+
     COLOR_MAP.put( "Other", "#7945D0" );
   }
 
   public static String getColourString( String aName ) {
       String color  = COLOR_MAP.getOrDefault( aName, "#FFFFFF" );
       if( "#FFFFFF".equals( color ) ){
+        // TODO use a logger and warn
         System.out.println( "Color unknown for: " + aName );
       }
       return color;
